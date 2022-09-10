0c8f391e25c85ae81eb81f3c68429a9f55346236
Anthony <29755086+anthony-be@users.noreply.github.com>
2022-09-10 11:03:24 +0200

    Added Rest PdfController + test

---
+++ src/main/java/be/cocoding/bubblepdf/web/controller/PdfController.java
@@ -0,0 +1,54 @@
+package be.cocoding.bubblepdf.web.controller;
+
+import be.cocoding.bubblepdf.model.PdfRequestWrapper;
+import be.cocoding.bubblepdf.parser.RequestJsonParser;
+import be.cocoding.bubblepdf.report.OpenPdfGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/pdf")
+public class PdfController {
+
+    private static final Logger logger = LoggerFactory.getLogger(PdfController.class);
+
+    @PostMapping
+    public ResponseEntity<byte[]> createPdf(@RequestBody String json){
+        //Parse json
+        PdfRequestWrapper pdfRequestWrapper = null;
+        try {
+            pdfRequestWrapper = RequestJsonParser.parseJson(json);
+        } catch (Exception e) {
+            logger.error("Failed to parse JSON body", e);
+        }
+        if(pdfRequestWrapper == null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        //Generate PDF
+        byte[] pdfContent;
+        try {
+            OpenPdfGenerator pdfGenerator = new OpenPdfGenerator();
+            ByteArrayOutputStream pdfOut = new ByteArrayOutputStream();
+            pdfGenerator.generatePdf(pdfRequestWrapper, pdfOut);
+            pdfOut.close();
+            pdfContent = pdfOut.toByteArray();
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+        // Return content as byte[]
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setCacheControl(CacheControl.noStore().mustRevalidate().cachePrivate());
+        return new ResponseEntity<>(pdfContent, headers, HttpStatus.OK);
+    }
+}
---
+++ src/test/java/be/cocoding/bubblepdf/web/controller/PdfControllerTest.java
@@ -0,0 +1,89 @@
+package be.cocoding.bubblepdf.web.controller;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+class PdfControllerTest {
+
+    private WebTestClient client;
+
+    @BeforeEach
+    void setUp() {
+        final int memSize = 20 * 1024 * 1024;
+
+        client = MockMvcWebTestClient.bindToController(new PdfController())
+                .configureClient()
+                    .baseUrl("/api/pdf")
+                    .exchangeStrategies(ExchangeStrategies.builder()
+                            .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(memSize))
+                            .build())
+                .build();
+    }
+
+    @Test
+    void createPdf_WithWrongHttpMethod() {
+        client.get()
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.METHOD_NOT_ALLOWED)
+                .expectBody().isEmpty();
+    }
+
+    @Test
+    void createPdf_WithEmptyPayload() {
+        client.post()
+                .body(BodyInserters.empty())
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.BAD_REQUEST)
+                .expectBody().isEmpty();
+    }
+
+    @Test
+    void createPdf_WithInvalidJsonPayload() {
+        String invalidJson = "{ invalid-key : invalid value with space not wrapped in double quotes }";
+        client.post().bodyValue(invalidJson).exchange()
+                .expectStatus().isEqualTo(HttpStatus.BAD_REQUEST)
+                .expectBody().isEmpty();
+    }
+
+    @Test
+    void createPdf() throws IOException {
+        String validJson = sampleJson();
+        client.post()
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(validJson)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK)
+                .expectHeader().contentType(MediaType.APPLICATION_PDF)
+                .expectHeader().cacheControl(CacheControl.noStore().mustRevalidate().cachePrivate())
+//                .expectBody().consumeWith(this::storeInFile)
+        ;
+    }
+
+    private String sampleJson() throws IOException {
+        Resource resource = new DefaultResourceLoader().getResource("classpath:/sample-request-with-images.json");
+        return IOUtils.toString(resource.getInputStream(), StandardCharsets.UTF_8);
+    }
+
+//    private void storeInFile(EntityExchangeResult<byte[]> result) {
+//        try {
+//            Path tempFile = Files.createTempFile("PdfControllerTest", ".pdf");
+//            System.out.println("Test output in file: " + tempFile.toAbsolutePath());
+//            Files.write(tempFile, result.getResponseBody());
+//        } catch (IOException e) {
+//            sneakyThrow(e);
+//        }
+//    }
+}
