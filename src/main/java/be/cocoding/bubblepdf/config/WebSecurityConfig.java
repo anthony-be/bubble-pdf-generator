2fe0cefcda1c68fff73aac5da343c923460da9d2
Anthony <29755086+anthony-be@users.noreply.github.com>
2022-09-11 20:49:54 +0200

    Added Security with Basic Authentication, based on system environment for credentials

---
+++ src/main/java/be/cocoding/bubblepdf/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package be.cocoding.bubblepdf.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Value("${AUTHENTICATION_USER}")
+    private String authenticationUser;
+
+    @Value("${AUTHENTICATION_PASSWORD}")
+    private String authenticationPassword;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic().authenticationEntryPoint(new Http403ForbiddenEntryPoint())
+                .and()
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        // The builder will ensure the passwords are encoded before saving in memory
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails user = users
+                .username(authenticationUser)
+                .password(authenticationPassword)
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+
+}
---
+++ src/test/java/be/cocoding/bubblepdf/web/WebAppSecurityTest.java
@@ -0,0 +1,88 @@
+package be.cocoding.bubblepdf.web;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Collections;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class WebAppSecurityTest {
+
+    private final String BASE_URI = "/api/pdf";
+
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    void testNoAuthentication() throws IOException {
+        client.post()
+                .uri(BASE_URI)
+                .bodyValue(sampleJson())
+                .headers(httpHeaders -> {
+                    // Make sure Authorization header is not set
+                    if(httpHeaders.containsKey(HttpHeaders.AUTHORIZATION)){
+                        httpHeaders.remove(HttpHeaders.AUTHORIZATION);
+                    }
+                })
+                .exchange()
+                .expectStatus().isForbidden();
+    }
+
+    @Test
+    void testEmptyAuthentication() throws IOException {
+        client.post()
+                .uri(BASE_URI)
+                .bodyValue(sampleJson())
+                .headers(httpHeaders -> httpHeaders.replace(HttpHeaders.AUTHORIZATION, Collections.singletonList("")))
+                .exchange()
+                .expectStatus().isForbidden();
+    }
+
+    @Test
+    void testWrongAuthorizationWithBearer() throws IOException {
+        client.post()
+                .uri(BASE_URI)
+                .bodyValue(sampleJson())
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(Base64.getEncoder().encodeToString("1234567890".getBytes(StandardCharsets.UTF_8))))
+                .exchange()
+                .expectStatus().isForbidden();
+    }
+
+    @Test
+    void testBasicAuthorizationWrongPassword() throws IOException {
+        client.post()
+                .uri(BASE_URI)
+                .bodyValue(sampleJson())
+                .headers(httpHeaders -> httpHeaders.setBasicAuth("nick", "nick"))
+                .exchange()
+                .expectStatus().isForbidden();
+    }
+
+    @Test
+    void testBasicAuthorizationWithValidCredentials() throws IOException {
+        client.post()
+                .uri(BASE_URI)
+                .bodyValue(sampleJson())
+                .headers(httpHeaders -> httpHeaders.setBasicAuth("nick", "password"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_PDF);
+    }
+
+    private String sampleJson() throws IOException {
+        Resource resource = new DefaultResourceLoader().getResource("classpath:/sample-request-with-images.json");
+        return IOUtils.toString(resource.getInputStream(), StandardCharsets.UTF_8);
+    }
+}
