1dc5c9266081bab640768c0e9a894f3b06690a4e
Anthony <29755086+anthony-be@users.noreply.github.com>
2022-11-29 06:39:06 +0100

    PdfController: added report store + logging statements

--- src/main/java/be/cocoding/bubblepdf/web/controller/PdfController.java
+++ src/main/java/be/cocoding/bubblepdf/web/controller/PdfController.java
@@ -1,11 +1,17 @@
 package be.cocoding.bubblepdf.web.controller;
 
+import be.cocoding.bubblepdf.model.Metadata;
 import be.cocoding.bubblepdf.model.PdfRequestWrapper;
 import be.cocoding.bubblepdf.parser.RequestJsonParser;
 import be.cocoding.bubblepdf.report.OpenPdfGenerator;
+import be.cocoding.bubblepdf.storage.ReportStore;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.ByteArrayOutputStream;
@@ -13,13 +19,24 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/pdf")
-public class PdfController {
+public class PdfController implements InitializingBean {
 
     private static final Logger logger = LoggerFactory.getLogger(PdfController.class);
 
+    private final ReportStore store;
+
+    @Autowired
+    public PdfController(ReportStore store) {
+        this.store = store;
+    }
+
     @PostMapping
-    public ResponseEntity<byte[]> createPdf(@RequestBody String json){
+    public ResponseEntity createPdf(@RequestBody String json){
+        logger.info("Handling request to generate pdf report...");
+        logger.info("Payload: {}", StringUtils.truncate(json, 140));
+
         //Parse json
+        logger.info("Parsing JSON payload...");
         PdfRequestWrapper pdfRequestWrapper = null;
         try {
             pdfRequestWrapper = RequestJsonParser.parseJson(json);
@@ -31,6 +48,7 @@ public class PdfController {
         }
 
         //Generate PDF
+        logger.info("Generating PDF report...");
         byte[] pdfContent;
         try {
             OpenPdfGenerator pdfGenerator = new OpenPdfGenerator();
@@ -39,19 +57,40 @@ public class PdfController {
             pdfOut.close();
             pdfContent = pdfOut.toByteArray();
         } catch (IOException e) {
+            logger.error("Failed to generate the PDF report", e);
             return ResponseEntity.internalServerError().build();
         }
 
+        Metadata metadata = pdfRequestWrapper.getMetadata();
+        if(metadata!=null && metadata.isCompleteForStorage()){
+            return storePdfAndReturnOkResponse(metadata, pdfContent);
+        }
 
-        // Return content as byte[]
+        // Return content as response payload
+        return pdfAsResponsePayload(pdfContent);
+    }
+
+    private ResponseEntity pdfAsResponsePayload(byte[] pdfContent){
+        logger.info("Returning PDF as HTTP response payload");
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
         headers.setCacheControl(CacheControl.noStore().mustRevalidate().cachePrivate());
         return new ResponseEntity<>(pdfContent, headers, HttpStatus.OK);
     }
+
+    private ResponseEntity storePdfAndReturnOkResponse(Metadata metadata, byte[] pdfContent){
+        store.store(metadata.getBucketId(), metadata.getPdfFileId(), pdfContent);
+        logger.info("PDF report stored, returning http response");
+        return ResponseEntity.noContent().build();
+    }
+
     @GetMapping(produces = {MediaType.TEXT_PLAIN_VALUE})
     public ResponseEntity<String> helloWorld(){
         return ResponseEntity.ok("Hello PDF World !");
     }
 
+    @Override
+    public void afterPropertiesSet() {
+        Assert.notNull(store, "Property 'store' should not be null");
+    }
 }
--- src/test/java/be/cocoding/bubblepdf/web/controller/PdfControllerTest.java
+++ src/test/java/be/cocoding/bubblepdf/web/controller/PdfControllerTest.java
@@ -1,8 +1,10 @@
 package be.cocoding.bubblepdf.web.controller;
 
+import be.cocoding.bubblepdf.storage.ReportStore;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.http.CacheControl;
@@ -16,15 +18,23 @@ import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
 class PdfControllerTest {
 
     private WebTestClient client;
 
+    private ReportStore mockReportStore;
+
     @BeforeEach
     void setUp() {
         final int memSize = 20 * 1024 * 1024;
+        mockReportStore = Mockito.mock(ReportStore.class);
+        PdfController pdfController = new PdfController(mockReportStore);
 
-        client = MockMvcWebTestClient.bindToController(new PdfController())
+        client = MockMvcWebTestClient.bindToController(pdfController)
                 .configureClient()
                     .baseUrl("/api/pdf")
                     .exchangeStrategies(ExchangeStrategies.builder()
@@ -60,7 +70,7 @@ class PdfControllerTest {
 
     @Test
     void createPdf() throws IOException {
-        String validJson = sampleJson();
+        String validJson = sampleJsonWithImage();
         client.post()
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(validJson)
@@ -72,8 +82,126 @@ class PdfControllerTest {
         ;
     }
 
-    private String sampleJson() throws IOException {
-        Resource resource = new DefaultResourceLoader().getResource("classpath:/sample-request-with-images.json");
+    @Test
+    void createPdfWithBucketAndPdfFileId() throws IOException {
+        Mockito.doNothing().when(mockReportStore).store(eq("bubble-kindred-tales-test"), eq("gdcerfthb34dr78"), any());
+
+        String validJson = sampleJsonWithBucket();
+        client.post()
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(validJson)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.NO_CONTENT);
+
+        verify(mockReportStore).store(eq("bubble-kindred-tales-test"), eq("gdcerfthb34dr78"), any());
+    }
+
+    @Test
+    void createPdfWithNoBucket() throws IOException {
+
+        String validJson = sampleJsonWithNoBucket();
+        client.post()
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(validJson)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK);
+
+    }
+
+    @Test
+    void createPdfWithEmptyBucket() throws IOException {
+
+        String validJson = sampleJsonWithEmptyBucket();
+        client.post()
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(validJson)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK);
+
+    }
+
+    @Test
+    void createPdfWithBlankBucket() throws IOException {
+
+        String validJson = sampleJsonWithBlankBucket();
+        client.post()
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(validJson)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK);
+
+    }
+
+    @Test
+    void createPdfWithNoFileId() throws IOException {
+
+        String validJson = sampleJsonWithNoFileId();
+        client.post()
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(validJson)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK);
+
+    }
+
+    @Test
+    void createPdfWithEmptyFileId() throws IOException {
+
+        String validJson = sampleJsonWithEmptyFileId();
+        client.post()
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(validJson)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK);
+
+    }
+
+    @Test
+    void createPdfWithBlankFileId() throws IOException {
+
+        String validJson = sampleJsonWithBlankFileId();
+        client.post()
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(validJson)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK);
+
+    }
+
+    private String sampleJsonWithImage() throws IOException {
+        return getSampleJson("sample-request-with-images.json");
+    }
+
+    private String sampleJsonWithBucket() throws IOException {
+        return getSampleJson("sample-request-with-bucket.json");
+    }
+
+    private String sampleJsonWithNoBucket() throws IOException {
+        return getSampleJson("sample-request-with-metadata-no-bucket.json");
+    }
+
+    private String sampleJsonWithEmptyBucket() throws IOException {
+        return getSampleJson("sample-request-with-metadata-empty-bucket.json");
+    }
+
+    private String sampleJsonWithBlankBucket() throws IOException {
+        return getSampleJson("sample-request-with-metadata-blank-bucket.json");
+    }
+
+    private String sampleJsonWithNoFileId() throws IOException {
+        return getSampleJson("sample-request-with-metadata-no-fileid.json");
+    }
+
+    private String sampleJsonWithEmptyFileId() throws IOException {
+        return getSampleJson("sample-request-with-metadata-empty-fileid.json");
+    }
+
+    private String sampleJsonWithBlankFileId() throws IOException {
+        return getSampleJson("sample-request-with-metadata-blank-fileid.json");
+    }
+
+    private String getSampleJson(String sampleFileName) throws IOException {
+        Resource resource = new DefaultResourceLoader().getResource("classpath:/" + sampleFileName);
         return IOUtils.toString(resource.getInputStream(), StandardCharsets.UTF_8);
     }
 
